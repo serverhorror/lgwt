59aed61360c451dcf7bc34af68657f66b09e8252
serverhorror <serverhorror@users.noreply.github.com>
2023-09-26 13:05:08 +0200

    (chore): refactor to add test helper

--- hello_world/main_test.go
+++ hello_world/main_test.go
@@ -6,19 +6,20 @@ func TestHelloWho(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := HelloWho("Chris")
 		want := "Hello Chris!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorretMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := HelloWho("")
 		want := "Hello World!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorretMessage(t, got, want)
 	})
+}
+
+func assertCorretMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
 
 func TestHelloWorld(t *testing.T) {
