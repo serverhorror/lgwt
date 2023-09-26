92b85b8f219d8172aa23eadca3f02c18c626f701
serverhorror <serverhorror@users.noreply.github.com>
2023-09-26 16:00:25 +0200

    (arrays/slices) Initial version

---
+++ arrays_and_slices/sum_test.go
@@ -0,0 +1,29 @@
+package arrays_and_slices
+
+import "testing"
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func TestSum(t *testing.T) {
+
+	testCases := []struct {
+		numbers  []int
+		expected int
+	}{
+		{[]int{1, 2, 3, 4, 5}, 15},
+		{[]int{1, 2, 3, 4, 5, 1}, 16},
+	}
+
+	for _, tc := range testCases {
+		got := Sum(tc.numbers)
+		if got != tc.expected {
+			t.Errorf("got %d want %d given, %v", got, tc.expected, tc.numbers)
+		}
+	}
+}
