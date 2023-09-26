aa7977abf10a19809a40483249876000c3688f0d
serverhorror <serverhorror@users.noreply.github.com>
2023-09-26 15:52:08 +0200

    (integers) add table tests

--- integers/add_test.go
+++ integers/add_test.go
@@ -3,10 +3,19 @@ package integers
 import "testing"
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
-
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	testCases := []struct {
+		a, b, expected int
+	}{
+		{2, 2, 4},
+		{1, 2, 3},
+		{0, 0, 0},
+		{-1, 1, 0},
+		{-1, -1, -2},
+	}
+	for _, tc := range testCases {
+		sum := Add(tc.a, tc.b)
+		if sum != tc.expected {
+			t.Errorf("expected '%d' but got '%d'", tc.expected, sum)
+		}
 	}
 }
