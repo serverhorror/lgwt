7f86f8d478e44cdaf6c20ba45a20c9b7684fde5f
serverhorror <serverhorror@users.noreply.github.com>
2023-09-26 15:41:44 +0200

    (chore): add internationalized HelloI18NWho

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func HelloI18nWho(who, lang string) (string, error) {
+	if who == "" {
+		who = "World"
+	}
+	if lang == "en" {
+		return "Hello " + who + "!", nil
+	} else if lang == "es" {
+		return "Hola " + who + "!", nil
+	} else if lang == "fr" {
+		return "Bonjour " + who + "!", nil
+	} else {
+		return "Hello " + who + "!", fmt.Errorf("Language %v is unknown!", lang)
+	}
+}
+
 func HelloWho(who string) string {
 	if who == "" {
 		who = "World"
--- hello_world/main_test.go
+++ hello_world/main_test.go
@@ -1,21 +1,40 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestI18NHelloWho(t *testing.T) {
+	got, err := HelloI18nWho("Alice", "en")
+	want := "Hello Alice!"
+	wantErr := true
+	assertCorrectMessageWithErr(t, got, want, err, wantErr)
+}
 
 func TestHelloWho(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := HelloWho("Chris")
 		want := "Hello Chris!"
-		assertCorretMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := HelloWho("")
 		want := "Hello World!"
-		assertCorretMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 }
 
-func assertCorretMessage(t testing.TB, got, want string) {
+func assertCorrectMessageWithErr(t testing.TB, got, want string, err error, wantErr bool) {
+	t.Helper()
+	if err != nil && !wantErr {
+		t.Errorf("got %q wantErr %v", err, wantErr)
+	}
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
