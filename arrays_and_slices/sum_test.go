6dbbc50e086606641e39947ca7292409f2856ce3
serverhorror <serverhorror@users.noreply.github.com>
2023-09-26 16:07:22 +0200

    (arrays/slices) refactor to move functions to their own file

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -2,22 +2,6 @@ package arrays_and_slices
 
 import "testing"
 
-func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
-}
-
-func SumAll(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers))
-	}
-	return sums
-}
-
 func TestSumAll(t *testing.T) {
 	testCases := []struct {
 		numbers  [][]int
