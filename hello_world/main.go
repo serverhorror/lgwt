c7a8feb2dbcbaf1bfbc76ef20be053df26bfef40
serverhorror <serverhorror@users.noreply.github.com>
2023-09-26 13:01:35 +0200

    (hello_world): added HelloWho(string)

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func HelloWho(who string) string {
+	return "Hello " + who + "!"
+}
+
 func HelloWorld() string {
 	return "Hello World!"
 }
--- hello_world/main_test.go
+++ hello_world/main_test.go
@@ -2,6 +2,14 @@ package main
 
 import "testing"
 
+func TestHelloWho(t *testing.T) {
+	got := HelloWho("World")
+	want := "Hello World!"
+	if got != want {
+		t.Errorf("HelloWho() = %q, want %q", got, want)
+	}
+}
+
 func TestHelloWorld(t *testing.T) {
 	want := "Hello World!"
 	got := HelloWorld()
